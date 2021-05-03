da738d343b83f858aa10cd81b36ef94ff431e8fb
Kateryna Shydlovska <katerynashydlovska@Katerynas-MacBook-Air.local>
2021-05-03 17:28:52 -0500

    deleted unnessesary prints

--- fundamentals/fundamentals/Students_and_Grades.py
+++ fundamentals/fundamentals/Students_and_Grades.py
@@ -1,35 +1,36 @@
 
 length = int(input("How many students do you have?"))
-students ={}
+listOfStudents = []
 
 while length:
+    students = {}
     name = input("Students name: ")
     if not name:
         print("Invalid name, try again..")
         name = input("Students name: ")
-    students[name] = {"Name": name}
+    students["name"] = name
     grade = input("Students garde: ")
     while not grade.isnumeric():
         print("Please enter the number")
         grade = input("Students garde: ")
     grade = int(grade)
-    students[name]["Grade"] = grade
-    print(grade)
+    students["Grade"] = grade
     course = input("Select a course: 1 - Math, 2 - Science, 3 - History: ")
     while not course.isnumeric():
         print("Please enter the number")
         course = input("Students garde: ")
     course = int(course)
     if course == 1:
-        students[name]["Course"] = 'Math'
+        students["Course"] = 'Math'
     elif course == 2:
-        students[name]["Course"] = 'Science'
+        students["Course"] = 'Science'
     elif course == 3:
-        students[name]["Course"] = 'History'
+        students["Course"] = 'History'
     else:
         input("Try again")       
-    print(course)
+    listOfStudents.append(students)
     length -=1
-    
-for val in students.values():
-    print(val)
+
+
+for el in listOfStudents:
+    print(el)
