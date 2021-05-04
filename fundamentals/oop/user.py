5d2e2781deeaba159b6c8645de5f53da1619a345
Kateryna Shydlovska <katerynashydlovska@Katerynas-MacBook-Air.local>
2021-05-04 16:08:51 -0500

    fixed methods

--- fundamentals/oop/user.py
+++ fundamentals/oop/user.py
@@ -5,17 +5,21 @@ class User:
         self.account_balance = 0
 
     def make_deposit(self, amount):	
-        self.account_balance += amount	
+        self.account_balance += amount
+        return self	
 
     def make_withdrawal(self, amount):	
-        self.account_balance -= amount	
+        self.account_balance -= amount
+        return self	
 
     def display_user_balance(self):
         print(self.name, self.account_balance)
+        return self	
         
     def transfer_money(self, other_user, amount):
         self.account_balance -= amount
         other_user.account_balance += amount
+        return self	
         
         
 
