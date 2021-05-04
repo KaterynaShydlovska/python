ba91e1b8a6955d9dcbc649e03cbdff8e3a4886bb
Kateryna Shydlovska <katerynashydlovska@Katerynas-MacBook-Air.local>
2021-05-04 10:27:49 -0500

    perentasis algo

--- morningAlgo/algo.js
+++ morningAlgo/algo.js
@@ -34,3 +34,67 @@ function reverseString(str) {
 }
 
 console.log(reverseString("creature")); // "erutaerc"
+
+
+// Create a function that, given an input string, returns a boolean whether parentheses in that string are valid.
+
+// Example 1:"y(3(p)p(3)r)s" --> true
+// Example 2: "n(0(p)3" --> false
+// Example 3: "n)0(t(o)k" --> false
+
+// hint: consider using an array or object to solve
+
+function parensValid(str) {
+    let arr = [];
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === "(") {
+            arr.push(str[i])
+        } else if (str[i] === ")") {
+            if (arr.length === 0) {
+                return false;
+            }
+            arr.pop()
+        }
+    }
+    if (arr.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+parensValid("()")
+
+
+// Given a string, returns whether the sequence of various parentheses, braces and brackets within it are valid. 
+
+// Example 1: "({[({})]})" --> true
+// Example 2: "d(i{a}l[t]o)n{e!" --> false
+// Example 2: "{{[]}}(){}{()}" --> true
+
+// hint: consider using an array or object to solve
+
+function bracesValid(str) {
+    let arr = [];
+
+    for (let i = 0; i < str.length; i++) {
+
+        if (str[i] === "(" || str[i] === "[" || str[i] === "{") {
+            arr.push(str[i])
+        } else if (str[i] === ")" && arr[arr.length - 1] === "(" || str[i] === "}" && arr[arr.length - 1] === "{" || str[i] === "]" && arr[arr.length - 1] === "[") {
+            arr.pop();
+        } else {
+            return false;
+        }
+    }
+
+    if (arr.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+bracesValid("d(i{a}l[t]o)n{e!")
